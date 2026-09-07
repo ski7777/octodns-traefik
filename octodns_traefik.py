8f28d1c13834d23b252c64abd123d43c3499de53
Raphael Jacob <r.jacob2002@gmail.com>
2026-09-07 19:53:58 +0000

    next

--- octodns_traefik.py
+++ octodns_traefik.py
@@ -23,7 +23,6 @@ class TraefikSource(BaseSource):
         if len(self.zones) > 0:
             self.log.warning("Zone-based config is currently not supported. Ignoring...")
         self.client = TraefikClient(traefik_api_url)
-        self.routers = asyncio.run(self.client.list_routers())
         routers = asyncio.run(self._get_routers())
         self.hosts = self._get_hosts(routers)
         self.log.info(self.hosts)
